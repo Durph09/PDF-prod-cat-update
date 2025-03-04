13f5927d78b657c0abf67a3f53dce45cead4eb1b
Durph09 <crmahaney9@gmail.com>
2025-03-04 07:24:02 -0500

    update postCatChng.

--- src/bigcommerce_api/postCatChng.mjs
+++ src/bigcommerce_api/postCatChng.mjs
@@ -1,20 +1,20 @@
 
 export async function postCatChng(postBody){
 
-const url ='https://www.hepsales.com/api/promo-cat/'
+const url =process.env.SITE_API_URL
 
 console.log('postBody from postCatChng: ', postBody)
 const requestOptions = {
 method: "POST",
 headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Basic ${process.env.HEPSALES_API_KEY}`,
+    'Authorization': `Basic ${process.env.SITE_API_KEY}`,
 
     'Accept': '*/*',
    
     'Connection': 'keep-alive',
     'Cookie': 'NEXT_LOCALE=en',
-    'Referer': 'http://www.hepsales.com/api/promo-cat',
+    'Referer': url,
     'Host': 'www.hepsales.com',
 },
 body: JSON.stringify(postBody),
